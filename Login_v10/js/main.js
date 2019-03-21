fe01f5c7c2ceb54305cca79ab8c8894c5e33233c
Abel Weldaregay <abelweldaregay@gmail.com>
2019-03-21 09:55:38 -0400

    front end for login and register

---
+++ Login_v10/js/main.js
@@ -0,0 +1,71 @@
+
+(function ($) {
+    "use strict";
+
+
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+        });
+    });
+
+    function validate (input) {
+        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+$('.customerButton').on('click', function(e){
+
+    //location.replace("file:///Users/abelweldareguy/Downloads/colorlib-regform-1/customerRegister.html");
+    window.location.href = "file:///Users/abelweldareguy/Downloads/colorlib-regform-1/customerRegister.html";
+});
+
+$('.providerButton').on('click', function(e){
+
+    window.location.href = "file:///Users/abelweldareguy/Downloads/colorlib-regform-1/providerRegistration.html";
+
+
+});
+
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
+    
+
+})(jQuery);
