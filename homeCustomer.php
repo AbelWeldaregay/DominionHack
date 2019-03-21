4bc0c6398cf7d80ef88d668278d6522897952d9a
Abel Weldaregay <abelweldaregay@gmail.com>
2019-03-21 16:27:14 -0400

    update links

---
+++ homeCustomer.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HomeService</title>
+  <link rel="stylesheet" href="./css/home.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+
+</head>
+<body>
+<nav class="navbar navbar-default navbar-fixed-top">
+                <div class="container">
+                  <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>                        
+                    </button>
+                    <a href="https://google.com" style="color:#660033"><b>Home Service</b><img src="icon.png" style="height:50px;"></a>
+                    
+                  </div>
+                  <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav navbar-right" id="tabs">
+                      <li><a href="http://qav2.cs.odu.edu/abel/DomHack/colorlib-regform-1/providerRegistration.html">Become a Professional</a></li>
+                      <li><a href="http://qav2.cs.odu.edu/abel/DomHack/Login_v10/index.html">Logout</a></li>
+                      <li><a href="">Blog</a></li>
+                    </ul>
+                  </div>
+    </div>
+</nav>
+
+</body>
+</html>
